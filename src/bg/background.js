45caad77b72a707ea1df4003b19c386b4cbe7be8
Jeff <jeffblightning@gmail.com>
2017-07-21 03:45:49 -0500

    Added proper site detection for Facebook and Twitter, a few emoji

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,7 +1,57 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function () {
-    return {redirectUrl: chrome.extension.getURL('images/pixel.png')};
-  }, {
-    urls: ["*://abs.twimg.com/emoji/v2*"]
-  }, ["blocking"]
-);
+// chrome.webRequest.onCompleted.addListener(
+//   function () {
+//     return {
+//       redirectUrl: chrome.extension.getURL('images/pixel.png')
+//     };
+//   }, {
+//     urls: ["*://abs.twimg.com/emoji/v2*"]
+//   }, ["blocking"]
+// );
+
+chrome.webRequest.onCompleted.addListener(function () {
+  // if (typeof changeInfo.status == 'string' && changeInfo.status == 'complete') {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      key: 'nomoji'
+    }, function (response) {;
+    });
+  });
+  // }
+}, {
+  urls: ["<all_urls>"]
+});
+
+chrome.webRequest.onCompleted.addListener(function () {
+  // if (typeof changeInfo.status == 'string' && changeInfo.status == 'complete') {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      key: 'nofacebookmoji'
+    }, function (response) {;
+    });
+  });
+  // }
+}, {
+  urls: ["https://www.facebook.com/*"]
+});
+
+chrome.webRequest.onCompleted.addListener(function () {
+  // if (typeof changeInfo.status == 'string' && changeInfo.status == 'complete') {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      key: 'notwittermoji'
+    }, function (response) {;
+    });
+  });
+  // }
+}, {
+  urls: ["https://twitter.com/*"]
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,17 +1,48 @@
-var elements = document.getElementsByTagName('*');
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+	if ((msg.key === 'nomoji')) {
 
-for (var i = 0; i < elements.length; i++) {
-	var element = elements[i];
+		console.log("is it working?");
+		var elements = document.getElementsByTagName('*');
+		for (var i = 0; i < elements.length; i++) {
+			var element = elements[i];
 
-	for (var j = 0; j < element.childNodes.length; j++) {
-		var node = element.childNodes[j];
+			for (var j = 0; j < element.childNodes.length; j++) {
+				var node = element.childNodes[j];
 
-		if (node.nodeType === 3) {
-			var text = node.nodeValue;
-			var replacedText = text.replace(/[\uD83C-\uDBFF\uDC00-\uDFFF]+/g, "");
-			if (replacedText !== text) {
-				element.replaceChild(document.createTextNode(replacedText), node);
+				if (node.nodeType === 3) {
+					var text = node.nodeValue;
+					var replacedText = text.replace(/[\uD83C-\uDBFF\uDC00-\uDFFF\u2600\u2764]+/g, "");
+					if (replacedText !== text) {
+						element.replaceChild(document.createTextNode(replacedText), node);
+					}
+				}
 			}
 		}
 	}
-}
+	if ((msg.key === "nofacebookmoji")) {
+		var allImg = document.getElementsByTagName("img"),
+			i = 0,
+			img;
+
+		var pattern = /https:\/\/www.facebook.com\/images\/emoji.php.*\.png/g;
+
+		while (img = allImg[i++]) {
+			if (img.src.match(pattern)) {
+				img.src = img.src.replace(pattern, chrome.extension.getURL("images/pixel.png"));
+			}
+		}
+	}
+	if ((msg.key === "notwittermoji")) {
+		var allImg = document.getElementsByTagName("img"),
+			i = 0,
+			img;
+
+		var pattern = /https:\/\/abs\.twimg\.com\/emoji.*\.png/g;
+
+		while (img = allImg[i++]) {
+			if (img.src.match(pattern)) {
+				img.src = img.src.replace(pattern, chrome.extension.getURL("images/pixel.png"));
+			}
+		}
+	}
+});
